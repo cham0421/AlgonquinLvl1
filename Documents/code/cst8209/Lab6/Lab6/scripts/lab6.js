93d13f60c2668deb3ad1a5bd4a9b3dd88d06d077
Waseem <cham0421@algonquinlive.com>
2019-09-14 17:20:14 -0400

    downloaded Lab6 for 8209

---
+++ Documents/code/cst8209/Lab6/Lab6/scripts/lab6.js
@@ -0,0 +1,25 @@
+//lab6 JavaScript file
+
+    book1 = {
+    author: "William Shakespeare",
+    title : "The Tempest",
+    genre : "Historical Fiction"
+    };
+
+    book2 = {
+    author: "Stephen King",
+    title : "The Shining",
+    genre : "Horror"
+    };
+
+    book3 = {
+    author: "Anne Frank",
+    title : "The Diary of Anne Frank",
+    genre : "Non-Fiction"
+    };
+
+var bookArray = new Array();
+
+bookArray[0] = book1;
+bookArray[1] = book2;
+bookArray[2] = book3;
