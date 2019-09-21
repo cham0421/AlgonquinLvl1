df7fe7cd72edf5af63bd90857a9bdff784c1e664
Waseem <cham0421@algonquinlive.com>
2019-09-21 17:11:34 -0400

    created a lab5 sql file

---
+++ Documents/code/cst8260/Lab5/lab5.sql
@@ -0,0 +1,14 @@
+-- Create a report that displays the Code of a country along with the number of cities of that country. (GROUP BY, COUNT)
+select countryCode, count(countryCode) as "Cities" from city  group by countryCode limit 9999;
+
+-- Restrict the previous query to the countries with more than 200 cities. (GROUP BY, HAVING)
+select countryCode, count(countryCode) as "Cities" from city  group by countryCode  having count(countryCode) > 200 limit 9999;
+
+-- Run the next query and explain what happens: 
+SELECT countrycode, COUNT(*) FROM city GROUP BY countrycode having COUNT(*) > 200
+-- Create a report that displays the Code of a country along with the number of cities of that country and the total population of these cities. (GROUP BY, COUNT,SUM)
+-- Create a report that displays the district, the code of a country along with the number of cities of that district. (GROUP BY on two fields, COUNT)
+-- Create a report that displays the the code of a country along with the number of cities of that country, the total population of these cities and the average population (GROUP BY, COUNT, SUM, AVG)
+-- Create a report that displays the district, the code of a country along with the number of cities of that district, the total population of these cities and the average population. Select only the district starting with 'a' (GROUP BY on two fields, COUNT, SUM, AVG, HAVING)
+-- Restrict the previous query to the districts with more than 10 cities. (HAVING, AND)
+-- Create a report that displays the number of cities in the table, the total population, the average population, the minimal population value and the maximal population value. (COUNT, SUM, AVG, MIN, MAX)
