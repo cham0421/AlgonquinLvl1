f28ef39f96b55ea70fb0680284e244289b071a22
Waseem <cham0421@algonquinlive.com>
2019-09-23 17:47:06 -0400

    attempt to lab6

--- Documents/code/cst8209/Lab6/Lab6/scripts/lab6.js
+++ Documents/code/cst8209/Lab6/Lab6/scripts/lab6.js
@@ -1,25 +1,45 @@
 //lab6 JavaScript file
 
-    book1 = {
-    author: "William Shakespeare",
-    title : "The Tempest",
-    genre : "Historical Fiction"
-    };
+book1 = {
+  author: "William Shakespeare",
+  title: "The Tempest",
+  genre: "Historical Fiction"
+};
 
-    book2 = {
-    author: "Stephen King",
-    title : "The Shining",
-    genre : "Horror"
-    };
+book2 = {
+  author: "Stephen King",
+  title: "The Shining",
+  genre: "Horror"
+};
 
-    book3 = {
-    author: "Anne Frank",
-    title : "The Diary of Anne Frank",
-    genre : "Non-Fiction"
-    };
-
+book3 = {
+  author: "Anne Frank",
+  title: "The Diary of Anne Frank",
+  genre: "Non-Fiction"
+};
 var bookArray = new Array();
 
 bookArray[0] = book1;
 bookArray[1] = book2;
 bookArray[2] = book3;
+
+function addBooks() {
+  for (let index = 0; index < 3; index++) {
+    let addAuthor = prompt("Add the author for book " + index + ":", "")
+    let addTitle = prompt("Add the title for book " + index + ":", "")
+    let addGenre = prompt("Add the genre for book " + index + ":", "")
+    if (addAuthor == "" || addTitle == "" || addGenre == "") {
+      alert("Entries can't be blank");
+      index--;
+      continue;
+    }
+    book = {
+      author: addAuthor,
+      title: addTitle,
+      genre: addGenre
+    };
+    bookArray.push(book);
+  }
+
+}
+addBooks();
