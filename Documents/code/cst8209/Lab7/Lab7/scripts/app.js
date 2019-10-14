127b38875a2e2ab0fbcf6428ce78d1466724e27a
Waseem <cham0421@algonquinlive.com>
2019-10-14 13:07:49 -0400

    started lab7 assignment

---
+++ Documents/code/cst8209/Lab7/Lab7/scripts/app.js
@@ -0,0 +1,27 @@
+var totalCost = 0
+var cart = []
+var brands = [
+  {
+    "brand":"extra",
+    "price": 1.50
+  },
+  {
+    "brand":"wrigley",
+    "price": 2.00
+  },
+  {
+    "brand":"trident",
+    "price": 1.25
+  },
+  {
+    "brand":"bubble-gum",
+    "price": 0.99
+  },
+]
+function addPrice() {
+
+}
+function clear() {
+  cart = []
+  totalCost = 0
+}
