46ccebccd2841a5a9648c0ab020e53e4efd80660
Waseem <cham0421@algonquinlive.com>
2019-11-04 09:41:29 -0500

    added validate code

--- Documents/code/cst8209/Lab8/Lab8/scripts/events.js
+++ Documents/code/cst8209/Lab8/Lab8/scripts/events.js
@@ -1,1 +1,8 @@
 //form element events
+
+document.getElementsByName("submit").addEventListener("click", function () {
+  validate.checkValid()
+});
+document.getElementsByName("clear").addEventListener("click", function () {
+  validate.clearField()
+});
--- Documents/code/cst8209/Lab8/Lab8/scripts/validate.js
+++ Documents/code/cst8209/Lab8/Lab8/scripts/validate.js
@@ -1,9 +1,67 @@
 //validate module
-var validate = (function(){
+var validate = (function () {
 
-    //create private and public functions/objects/variables to validate the form
+  function validFname(params) {
+    if (document.getElementById(fname).innerText() == " ")
+      return false;
+    return true;
+  }
+
+  function validLname(params) {
+    if (document.getElementById(Lname).innerText() == " ")
+      return false;
+    return true;
+  }
+
+  function validAddress(params) {
+    if (document.getElementById(city).innerText() == " ")
+      return false;
+    return true;
+  }
+
+  function validCity(params) {
+    if (document.getElementById(city).innerText() == " ")
+      return false;
+    return true;
+  }
+
+  function validProvince(params) {
+    if (document.getElementById(province).innerText() == " ")
+      return false;
+    return true;
+  }
+
+  function validCountry(params) {
+    if (document.getElementById(country).innerText() == " ")
+      return false;
+    return true;
+  }
+  //create private and public functions/objects/variables to validate the form
+  function checkValid(params) {
+    if (validFname() &&
+      validLname() &&
+      validAddress() &&
+      validCity() &&
+      validProvince() &&
+      validCountry())
+      return true
+    return false
+  }
+  function clearField(params) {
+    document.getElementById(fname).innerText() = " "
+    document.getElementById(Lname).innerText() = " "
+    document.getElementById(address1).innerText() = " "
+    document.getElementById(city).innerText() = " "
+    document.getElementById(country).innerText() = " "
+    document.getElementById(province).innerText() = " "
+  }
+
+
+  return{
+    checkValid,
+    clearField
+  } 
     
-    
-    
-    
+
 }());
+
