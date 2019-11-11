61121141abc1cdcaabc5b50fb0015bb367282a68
Waseem <cham0421@algonquinlive.com>
2019-11-11 12:47:43 -0500

    commented

--- Documents/code/cst8279/Lab10/Lab10.py
+++ Documents/code/cst8279/Lab10/Lab10.py
@@ -2,8 +2,10 @@ import sqlite3
 import base64
 import webbrowser
 
+# Retreives data from a database and updates it
 def Lab10():
   choice = 0
+  
   # Keeps looping until the choice is 1-24 or 'q'
   while (choice < 1 or choice > 24):
     choice = input("Pick a number between 1-24: ")
@@ -14,6 +16,7 @@ def Lab10():
     except:
       choice = 0
       continue
+
   # Connect to the database
   # If can't connect exits function
   try:
@@ -25,12 +28,16 @@ def Lab10():
 
   # Retrieves the encoded data
   data = cursor.execute("SELECT Link FROM Lab10 WHERE id={}".format(choice)).fetchall()
+
   # Decodes the data
   url = base64.b64decode(data[0][0])
+  print(url)
+
   # Opens in web browser
-  webbrowser.open_new_tab(url)
+  webbrowser.open(url)
   city = input("What is the name of the city: ")
   country = input("What is the name of the country: ")
+
   # Updates the cell information and commits to database  
   cursor.execute("UPDATE Lab10 SET City = '{}' , Country = '{}' where id ={}".format(city,country,choice))
   conn.commit()
