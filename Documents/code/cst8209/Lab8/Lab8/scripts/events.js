5c3bd23a8960d520699fd17db2218fa2cd6d28c2
Waseem <cham0421@algonquinlive.com>
2019-11-19 22:41:20 -0500

    commented code

--- Documents/code/cst8209/Lab8/Lab8/scripts/events.js
+++ Documents/code/cst8209/Lab8/Lab8/scripts/events.js
@@ -1,35 +1,48 @@
 
+//remove warning if firstName has input
+document.getElementById("firstName").addEventListener("blur", function () {
+  if (this.value !== "") {
+    firstNameError.innerHTML = "";
+  }
+});
 
 //remove warning if lastName has input
-document.getElementById("firstName").addEventListener("blur", function(){
-  if(this.value !== "") {
-      firstNameError.innerHTML = "";
+document.getElementById("lastName").addEventListener("blur", function () {
+  if (this.value !== "") {
+    lastNameError.innerHTML = "";
   }
 });
-document.getElementById("lastName").addEventListener("blur", function(){
-  if(this.value !== "") {
-      lastNameError.innerHTML = "";
+
+//remove warning if address1 has input
+document.getElementById("address1").addEventListener("blur", function () {
+  if (this.value !== "") {
+    address1Error.innerHTML = "";
   }
 });
-document.getElementById("address1").addEventListener("blur", function(){
-  if(this.value !== "") {
-      address1Error.innerHTML = "";
+
+//remove warning if city has input
+document.getElementById("city").addEventListener("blur", function () {
+  if (this.value !== "") {
+    cityError.innerHTML = "";
   }
 });
-document.getElementById("city").addEventListener("blur", function(){
-  if(this.value !== "") {
-      cityError.innerHTML = "";
+
+//remove warning if province has been selected
+document.getElementById("province").addEventListener("blur", function () {
+  if (profile.province.options.selectedIndex != 0) {
+    provinceError.innerHTML = "";
   }
 });
-document.getElementById("province").addEventListener("blur", function(){
-  if(profile.province.options.selectedIndex !=0) {
-      provinceError.innerHTML = "";
+
+//remove warning if country has been selected
+document.getElementById("country").addEventListener("blur", function () {
+  if (profile.country.options.selectedIndex != 0) {
+    countryError.innerHTML = "";
   }
 });
-document.getElementById("country").addEventListener("blur", function(){
-  if(profile.country.options.selectedIndex !=0) {
-      countryError.innerHTML = "";
-  }
-});
-document.profile.addEventListener("submit", validate)
-document.profile.addEventListener("clear", clear)
+
+//added event listener for the submit button to check validation
+document.profile.addEventListener("submit", validate.validateProfile)
+
+//added event listener for the clear button to clear values
+document.profile.addEventListener("clear", validate.clear)
--- Documents/code/cst8209/Lab8/Lab8/scripts/validate.js
+++ Documents/code/cst8209/Lab8/Lab8/scripts/validate.js
@@ -1,46 +1,55 @@
 //validate module
-var validate = function (e) {
+var validate = (function () {
 
-  e.preventDefault()
-  var valid = true
+  // Checks that each form has contents in them 
+  var validateProfile = function (e) {
+    e.preventDefault()
+    var valid = true
 
-  if (profile.firstName.value == ""){
-    document.getElementById('firstNameError').innerHTML="*Please enter a First Name*";
-    valid = false;
-  }
-  
-  if (profile.lastName.value == ""){
-    document.getElementById('lastNameError').innerHTML="*Please enter a Last Name*";
-    valid = false;
-  }
-  
-  if (profile.address1.value == ""){
-    document.getElementById('address1Error').innerHTML="*Please enter an Address";
-    valid = false;
-  }
-  if (profile.city.value == ""){
-    document.getElementById('cityError').innerHTML="*Please enter an City";
-    valid = false;
-  }
-  
-  if (profile.province.options.selectedIndex == 0){
-    document.getElementById('provinceError').innerHTML="*Please select a province";
-    valid = false;
-  }
-  if (profile.country.options.selectedIndex == 0){
-    document.getElementById('countryError').innerHTML="*Please select a country";
-    valid = false;
+    if (profile.firstName.value == "") {
+      document.getElementById('firstNameError').innerHTML = "*Please enter a First Name*";
+      valid = false;
+    }
+
+    if (profile.lastName.value == "") {
+      document.getElementById('lastNameError').innerHTML = "*Please enter a Last Name*";
+      valid = false;
+    }
+
+    if (profile.address1.value == "") {
+      document.getElementById('address1Error').innerHTML = "*Please enter an Address";
+      valid = false;
+    }
+    if (profile.city.value == "") {
+      document.getElementById('cityError').innerHTML = "*Please enter an City";
+      valid = false;
+    }
+
+    if (profile.province.options.selectedIndex == 0) {
+      document.getElementById('provinceError').innerHTML = "*Please select a province";
+      valid = false;
+    }
+    if (profile.country.options.selectedIndex == 0) {
+      document.getElementById('countryError').innerHTML = "*Please select a country";
+      valid = false;
+    }
+    // If all forms are valid displays a thank you
+    if (valid)
+      alert("Thank You")
+    return valid;
   }
 
-  return valid;
-  
-};
-
-var clear = function (e) {
-  profile.fname.value == " "
-  profile.lname.value == " "
-  profile.address1.value == " "
-  profile.city.value == " "
-  profile.province.options.selectedIndex === -1
-  profile.country.options.selectedIndex === -1
-}
+  //clears the values of the form
+  var clear = function (e) {
+    profile.fname.value = ""
+    profile.lname.value = ""
+    profile.address1.value = ""
+    profile.city.value = ""
+    profile.province.options.selectedIndex = 0
+    profile.country.options.selectedIndex = 0
+  }
+  return {
+    validateProfile,
+    clear
+  }
+}());
