ec4b023af553319b136a48d7d860f9e5334952e7
Waseem <cham0421@algonquinlive.com>
2019-11-23 19:21:18 -0500

    got dropdown to work.......kinda

--- Documents/code/cst8279/Lab11/Lab11/lab11.py
+++ Documents/code/cst8279/Lab11/Lab11/lab11.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import modules
 # Create the application.
 APP = Flask(__name__)
@@ -11,13 +11,22 @@ def index():
 
 @APP.route('/table')
 def table():
-  
   return render_template('table.html',students = table_list)
-@APP.route('/map')
 
-def map():
-  
-  return render_template('map.html', students = table_list)
+@APP.route('/dropdown')
+def dropdown():  
+  return render_template('dropdown.html', students = table_list)
+
+@APP.route('/map', methods=['GET', 'POST'])
+def map():  
+  if request.method == "POST":
+    car_brand = request.form.get("student-dropdown", None)
+    if car_brand!=None:
+      for student in table_list:
+        if str(student['id']) == str(car_brand):
+          return render_template("map.html", data = student)
+    return render_template("map.html", data = " ")
+  return render_template("map.html", data = " ")
 
 
 if __name__ == '__main__':
