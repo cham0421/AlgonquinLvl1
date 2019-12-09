dff3f4bcaeaddde9eb6964cc43b6490ee193f80a
Waseem <cham0421@algonquinlive.com>
2019-12-09 15:05:02 -0500

    put some index reasons

--- Documents/code/cst8260/Lab8/Lab8.sql
+++ Documents/code/cst8260/Lab8/Lab8.sql
@@ -1,22 +1,42 @@
-SELECT 
-    city.name as cName, city.district, city.population, country.Code, country.name, country.Continent, country.Region, country.GNP, country.SurfaceArea
-FROM
-    city
-        LEFT JOIN
-    country ON city.CountryCode = country.code
-LIMIT 8999; 
+-- View 1 (base)
+CREATE VIEW base AS
+    SELECT 
+        city.name AS cName,
+        city.district,
+        city.population,
+        country.Code,
+        country.name,
+        country.Continent,
+        country.Region,
+        country.GNP,
+        country.SurfaceArea
+    FROM
+        city
+            LEFT JOIN
+        country ON city.CountryCode = country.code
+    LIMIT 8999; 
 
+-- 2 (Part 1)
 SELECT 
-   c.Continent, c.Region, c.name, c.GNP/(select count(city.name) from city where city.CountryCode = c.Code ) as average_gnp_per_city,c.GNP/c.SurfaceArea as average_gnp_per_squarekilometer
+    c.Continent,
+    c.Region,
+    c.name,
+    c.GNP / (SELECT 
+            COUNT(city.name)
+        FROM
+            city
+        WHERE
+            city.CountryCode = c.Code) AS average_gnp_per_city,
+    c.GNP / c.SurfaceArea AS average_gnp_per_squarekilometer
 FROM
-    country as c
-where (select sum(percentage) from countrylanguage where CountryCode = c.code and IsOfficial = true    ) > 60
-LIMIT 8999; 
-
-
-SELECT 
-   c.Continent, c.Region, c.name, c.GNP/(select count(city.name) from city where city.CountryCode = c.Code ) as average_gnp_per_city,c.GNP/c.SurfaceArea as average_gnp_per_squarekilometer
-FROM
-    country as c
-where (select sum(percentage) from countrylanguage where CountryCode = c.code and IsOfficial = true    ) > 60
-LIMIT 8999; 
+    country AS c
+WHERE
+    (SELECT 
+            SUM(percentage)
+        FROM
+            countrylanguage
+        WHERE
+            CountryCode = c.code
+                AND IsOfficial = TRUE) > 60 and 
+ORDER BY average_gnp_per_squarekilometer DESC
+lIMIT 10; 
