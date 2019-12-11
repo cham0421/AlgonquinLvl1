6778eebd53f37798d544ce89da56ac0f424aeae1
Waseem <cham0421@algonquinlive.com>
2019-12-11 00:21:28 -0500

    completed 8 without views

--- Documents/code/cst8260/Lab8/Lab8.sql
+++ Documents/code/cst8260/Lab8/Lab8.sql
@@ -21,13 +21,14 @@ SELECT
     c.Continent,
     c.Region,
     c.name,
-    c.GNP / (SELECT 
-            COUNT(city.name)
-        FROM
-            city
-        WHERE
-            city.CountryCode = c.Code) AS average_gnp_per_city,
-    c.GNP / c.SurfaceArea AS average_gnp_per_squarekilometer
+    ROUND(c.GNP / (SELECT 
+                    COUNT(*)
+                FROM
+                    city
+                WHERE
+                    city.CountryCode = c.Code),
+            2) AS average_gnp_per_city,
+    ROUND(c.GNP / c.SurfaceArea, 2) AS average_gnp_per_squarekilometer
 FROM
     country AS c
 WHERE
@@ -37,6 +38,97 @@ WHERE
             countrylanguage
         WHERE
             CountryCode = c.code
-                AND IsOfficial = TRUE) > 60 and 
+                AND IsOfficial = TRUE) > 60
+        AND (SELECT 
+            MAX(city.Population / country.population) * country.GNP AS max_city
+        FROM
+            country
+                LEFT JOIN
+            city ON city.CountryCode = country.code
+        WHERE
+            country.Code = c.Code
+        GROUP BY country.name) > (SELECT 
+            MIN(country.GNP)
+        FROM
+            country)
 ORDER BY average_gnp_per_squarekilometer DESC
-lIMIT 10; 
+LIMIT 10; 
+
+-- 2 (Part 2)
+    SELECT 
+    c.Continent,
+    c.Region,
+    c.name,
+    ROUND(c.GNP / (SELECT 
+                    COUNT(*)
+                FROM
+                    city
+                WHERE
+                    city.CountryCode = c.Code),
+            2) AS average_gnp_per_city,
+    ROUND(c.GNP / c.SurfaceArea, 2) AS average_gnp_per_sq_km,
+    c.GNP / c.population AS average_gnp_per_person
+FROM
+    country AS c
+WHERE
+    (SELECT 
+            SUM(percentage)
+        FROM
+            countrylanguage
+        WHERE
+            CountryCode = c.code
+                AND IsOfficial = TRUE) > 60
+        AND (SELECT 
+            MAX(city.Population / country.population) * country.GNP AS max_city
+        FROM
+            country
+                LEFT JOIN
+            city ON city.CountryCode = country.code
+        WHERE
+            country.Code = c.Code
+        GROUP BY country.name) > (SELECT 
+            MIN(country.GNP)
+        FROM
+            country)
+ORDER BY average_gnp_per_person DESC
+LIMIT 10; 
+
+-- 2 (Part 3)
+    SELECT 
+    c.Continent,
+    c.Region,
+    c.name,
+    ROUND(c.GNP / (SELECT 
+                    COUNT(*)
+                FROM
+                    city
+                WHERE
+                    city.CountryCode = c.Code),
+            2) AS average_gnp_per_city,
+    ROUND(c.GNP / c.SurfaceArea, 2) AS average_gnp_per_sq_km,
+    c.GNP / c.population AS average_gnp_per_person,
+    (select count(*) from countrylanguage where countrylanguage.CountryCode = c.Code) as number_languages
+FROM
+    country AS c
+WHERE
+    (SELECT 
+            SUM(percentage)
+        FROM
+            countrylanguage
+        WHERE
+            CountryCode = c.code
+                AND IsOfficial = TRUE) > 60
+        AND (SELECT 
+            MAX(city.Population / country.population) * country.GNP AS max_city
+        FROM
+            country
+                LEFT JOIN
+            city ON city.CountryCode = country.code
+        WHERE
+            country.Code = c.Code
+        GROUP BY country.name) > (SELECT 
+            MIN(country.GNP)
+        FROM
+            country)
+ORDER BY average_gnp_per_person DESC
+LIMIT 10; 
